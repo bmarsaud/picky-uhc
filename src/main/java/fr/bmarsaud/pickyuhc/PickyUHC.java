f0dff45e81213f6c133720bb1dc71fa5129dd2a0
Bastien Marsaud <bastien@mouseover.fr>
2021-04-14 23:28:55 +0200

    feat: load config at startup

--- src/main/java/fr/bmarsaud/pickyuhc/PickyUHC.java
+++ src/main/java/fr/bmarsaud/pickyuhc/PickyUHC.java
@@ -3,9 +3,38 @@ package fr.bmarsaud.pickyuhc;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class PickyUHC extends JavaPlugin implements Listener {
+    private List<String> enabledPlayers;
+    private List<String> disabledPlayers;
+    private boolean enabledByDefault;
+
     @Override
     public void onEnable() {
-        getLogger().info("Hello PickyUHC!");
+        saveDefaultConfig();
+        loadConfig();
+
+        getLogger().info("Natural regeneration is " + (enabledByDefault ? "enabled" : "disabled") + " by default");
+        getLogger().info(enabledPlayers.size() + " players loaded with natural regeneration enabled");
+        getLogger().info(disabledPlayers.size() + " players loaded with natural regeneration disabled");
+    }
+
+    public void loadConfig() {
+        enabledPlayers = getConfig().getStringList("regen.enable");
+        disabledPlayers = getConfig().getStringList("regen.disable");
+        enabledByDefault = enabledPlayers.contains("*");
+    }
+
+    public List<String> getEnabledPlayers() {
+        return enabledPlayers;
+    }
+
+    public List<String> getDisabledPlayers() {
+        return disabledPlayers;
+    }
+
+    public boolean isEnabledByDefault() {
+        return enabledByDefault;
     }
 }
